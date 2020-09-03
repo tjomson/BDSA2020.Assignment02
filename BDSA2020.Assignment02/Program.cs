fe7e0fe45d18b2107b76bdffb6076371062b23b9
Rasmus Lystrøm <rasmusl@microsoft.com>
2020-09-03 22:10:15 +0200

    Initial

---
+++ BDSA2020.Assignment02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDSA2020.Assignment01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BDSA2020.Assignment02/Wizard.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using System.Linq;
+using System.Globalization;
+
+namespace BDSA2020.Assignment02
+{
+    public class Wizard
+    {
+        public string Name { get; set; }
+
+        // Book or film or...
+        public string Medium { get; set; }
+
+        public int? Year { get; set; }
+
+        public string Creator { get; set; }
+
+        public static Lazy<IReadOnlyCollection<Wizard>> Wizards { get; } = new Lazy<IReadOnlyCollection<Wizard>>(() =>
+        {
+            var csv = File.OpenText("../../../../Wizards.csv");
+            using var reader = new CsvReader(csv, CultureInfo.InvariantCulture);
+            reader.Configuration.Delimiter = ",";
+            return reader.GetRecords<Wizard>().ToList().AsReadOnly();
+        });
+    }
+}
